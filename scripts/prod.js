273c87e095d7af59ca3323cf8996e084cbfde712
crong guy <yahtzeemaster26@gmail.com>
2020-07-03 08:31:42 -0600

    oops i did it again

--- scripts/prod.js
+++ scripts/prod.js
@@ -2,6 +2,8 @@
 function getIncrementorProd() {
   return D.pow(2, game.baseReductions)
     .times(game.incrementors.amount.times(nD(0.1).plus(nD(0.01).times(game.baseReductions.times((milestonesCompleted() >= 4) - 0)))
-      .times((milestonesCompleted() >= 1) - 0)))
+      .times((milestonesCompleted() >= 1) - 0))
       .plus(1)
+    )
+      
 }
